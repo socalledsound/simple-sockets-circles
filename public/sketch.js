7a2a90b4a843f98c5b2ff06355c593896a36c734
ck <ck@cks-macbook-pro.lan>
2022-10-09 12:59:46 -0700

    local player and player manager

--- public/sketch.js
+++ public/sketch.js
@@ -1,26 +1,26 @@
-
-
 const socket = io()
+const playerManager = new PlayerManager()
+const localPlayer = playerManager.createPlayer()
 
-let circle
-let circles = []
 
 function setup(){
     createCanvas(600, 600)
-    //frameRate(10)
-    // const pink = [220, 10, 200]
-    const randomCol = [random(100,255), random(100, 255), random(100, 255)]
-    circle = new Circle(300, 200, randomCol)
-    circles.push(circle)
+
 }
 
 function draw(){
     background(43)
-    circle.update()
-    circle.checkEdges()
-    circle.render()
+    if(localPlayer){
+        localPlayer.update()
+        localPlayer.checkEdges()
+        localPlayer.render()
+    }
+
 }
 
 function mouseMoved(){
-    circle.moveToMouse(mouseX, mouseY)
+    if(localPlayer){
+        localPlayer.moveToMouse(mouseX, mouseY)
+    }
+    
 }
